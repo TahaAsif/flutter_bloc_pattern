d305536952c8417f2cf69173ca86378db6b21535
Taha Asif Siddiqui <Taha.Asif@tpsonline.com>
2020-10-05 13:59:59 +0500

    refactored code

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_bloc_pattern/model/gallery_images.dart';
-import 'service_handler.dart';
+import '../service_handler/service_handler.dart';
 
 class Repository {
   ApiProvider appApiProvider = ApiProvider();
